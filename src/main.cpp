ffc2943f71cd1508113a465485daab857e099d7d
chenota <achenot@mines.edu>
2025-02-14 13:33:35 -0700

    feat: Added argument parsing to program

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
 int main(int argc, char *argv[]) {
+    // Argument parsing
+    enum {BASIC, EXTENDED} algorithm = BASIC;
+    enum {REGULAR, VERBOSE} mode = REGULAR;
+    int c;
+    while((c = getopt(argc, argv, "ev")) != -1) {
+        switch(c) {
+            // -e switches mode to extended
+            case 'e':
+                algorithm = EXTENDED;
+                break;
+            // -v switches output mode to verbose
+            case 'v':
+                mode = VERBOSE;
+                break;
+            // Unknown option error
+            default:
+                std::cerr << "Unknown option: " << c << std::endl;
+                exit(1);
+        }
+    }
+    // A and B inputs to algorithm
+    int a, b;
+    // Check positional arguments
+    if(argc - optind != 2) {
+        std::cerr << "Expected positional arguments A and B" << std::endl;
+        exit(1);
+    }
+    // Get A and B
+    a = std::atoi(argv[optind]);
+    b = std::atoi(argv[optind + 1]);
+    // Exit success
     return 0;
 }
